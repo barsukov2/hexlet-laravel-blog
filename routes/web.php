88f7bd970955b6df65b435222c558d427df61e0c
andrey.barsukov <andrey.barsukov@optimax.dev>
2020-07-31 18:24:03 +0400

    add controllers and migrations

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about', 'PageController@about');
+
+Route::get('/articles', 'ArticleController@index')
+    ->name('articles.index');
